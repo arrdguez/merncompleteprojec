8919207c59ea258101c5d4a106b6b8c8543da817
Alejandro Rodriguez <alerrdguez88@gmail.com>
2021-06-21 20:50:09 -0500

    New Feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return  <Router>
+            <Switch>
+               <Route />
+            </Switch>
+          </Router>;
+
 }
 
 export default App;
